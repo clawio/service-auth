a6f364de75e10fa9c012363041fb52fe42e1c919
Hugo Gonzalez <github@hugo.labkode.com>
2016-03-20 22:39:53 +0100

    [server] added docu to tokenstore

--- server/tokenstore/tokenstore.go
+++ server/tokenstore/tokenstore.go
@@ -4,6 +4,9 @@ import (
 	"github.com/clawio/service-auth/server/spec"
 )
 
+// TokenStore represents a store that can handle creation of tokens from an identity
+// and also verify issued tokens.
+// Implementations are: JWT
 type TokenStore interface {
 	Create(identity *spec.Identity) (string, error)
 	Verify(token string) (*spec.Identity, error)
